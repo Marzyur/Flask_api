77d88cf900060eba525c4f8b6058262646feb7fc
Mayur pawar <mbpawar@mitaoe.ac.in>
2023-02-21 17:38:11 +0530

    port

--- app.py
+++ app.py
@@ -25,4 +25,4 @@ def generate():
     return send_file('output.mp3', as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True,host='127.0.0.1'port=5000)
+    app.run(debug=True,host='127.0.0.1',port=5000)
